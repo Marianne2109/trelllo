f7559b32df50ee13642994b09e981f80996127d8
Marianne2109 <15081@coderacademy.edu.au>
2024-06-15 10:50:05 +1000

    Add cli controllers

--- main.py
+++ main.py
@@ -10,10 +10,13 @@ def create_app():
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 
     app.config["JWT_SECRET_KEY"] = os.environ.get("JWT_SECRET_KEY")
-    
+
     db.init_app(app)
     ma.init_app(app)
     bcrypt.init_app(app)
     jwt.init_app(app)
-    
+
+    from controllers.cli_controller import db_commands
+    app.register_blueprint(db_commands)
+
     return app
